f465fad071db6bf690250a0e733532043d10b4ec
thomas maestas <thomasm1.maestas@gmail.com>
2019-10-20 23:25:49 -0600

    serverless: tm-lambda-cloudformation w & w/out deps

---
+++ Serverless/README.md
@@ -0,0 +1,4 @@
+### lambda -thomasm1 + thomas76
+### lambda-api-gateway -thomasm1
+### lambda-cloudformation - thomasm1
+### lambda-xray -thomasm1
---
+++ Serverless/tm-lambda/index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+    
+    
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('tm-Lambda 2'),
+    };
+    return response;
+};
