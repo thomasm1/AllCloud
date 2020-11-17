372aa56a0c1d1620d4aec4f2dde91e8bef602e9c
thomas maestas <thomasm1.maestas@gmail.com>
2020-11-16 23:22:54 -0500

    _AWS: lambda, cloudformation elasticbeanstalk scripts

--- _AWS/powershell/route53_powershell/credentials.ps1
+++ _AWS/powershell/route53_powershell/credentials.ps1
@@ -1,6 +1,6 @@
 #Replace the values below and save this file as credentials.ps1
-$AWSAccessKey="WHZA" # Replace with your access key
-$AWSSecretKey="KE6w+R" # Replace with your secret key
+$AWSAccessKey="*****WHZA" # Replace with your access key
+$AWSSecretKey="******KE6w+R" # Replace with your secret key
 
 # Set AWS credentials and region
 $AWSProfileName="aws-networking-deep-dive-route-53-dns" # "dailytechfilter" # 
